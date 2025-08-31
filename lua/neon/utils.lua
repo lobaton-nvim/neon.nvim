ebbdaba1aa504c0c8e5d192204ef267dfbbc31a0
eliezer lobaton <eliezerlobaton39@gmail.com>
2025-08-31 01:52:36 -0300

    finally

--- lua/neon/utils.lua
+++ lua/neon/utils.lua
@@ -4,7 +4,7 @@ function M.hl(group, style)
 	vim.api.nvim_set_hl(0, group, style)
 end
 
-function M.loaad_highlights(groups)
+function M.load_highlights(groups)
 	for group, style in pairs(groups) do
 		M.hl(group, style)
 	end
